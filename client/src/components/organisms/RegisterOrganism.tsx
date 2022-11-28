31b00a687cdb7bc6ab0de9455cf5125e1f0f541f
JulesEfrei <jules.bruzeau@efrei.net>
2022-11-28 16:58:55 +0100

    Add : Register Front

--- client/src/components/organisms/RegisterOrganism.tsx
+++ client/src/components/organisms/RegisterOrganism.tsx
@@ -3,9 +3,12 @@ import React from "react";
 import { RegisterTemplate } from "../Templates/";
 import FormTypes from "../../types/formType";
 import { verifForm, generateToast } from "../../utils/formVerification";
+import { useNavigate } from "react-router";
 
 const RegisterOrganism: React.FC = () => {
-  const verif = (data: FormTypes) => {
+  const navigate = useNavigate();
+
+  const verif = async (data: FormTypes) => {
     if (data.password !== data.passwordVerif) {
       generateToast("Error !", "error");
     } else {
@@ -14,6 +17,24 @@ const RegisterOrganism: React.FC = () => {
       } else {
         generateToast("Success !", "success");
         //CREATE ACCOUNT
+
+        const req = await fetch(`http://10.3.228.8:8080/auth/register`, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: data.name,
+            email: data.email,
+            password: data.password,
+          }),
+        });
+        const res = await req.json();
+
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
       }
     }
   };
